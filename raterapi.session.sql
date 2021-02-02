ed0be2ab95cd1196f87180c2be9e867d9428fa62
Matt Logan <mlogan5212@gmail.com>
2021-02-01 20:53:22 -0600

    number of games per category report

--- raterapi.session.sql
+++ raterapi.session.sql
@@ -1,11 +1,8 @@
                 SELECT
-                    g.title Title,
-                    AVG(r.rating) AverageRating,
-                    g.id game_id
-                FROM
-                    raterapi_game g
-                JOIN
-                    raterapi_ratings r ON r.game_id = g.id
-                GROUP BY g.id
-                ORDER BY AverageRating DESC
-                LIMIT 5
+                    gc.id,
+                    c.name CategoryName,
+                    COUNT(g.id) NumberGames
+                FROM raterapi_categories c 
+                JOIN raterapi_game_categories gc ON gc.categories_id = c.id
+                JOIN raterapi_game g ON g.id = gc.game_id
+                GROUP BY c.id
