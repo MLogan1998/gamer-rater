9dde4c6353a142be1b22f6744629c96fd7b918a6
Matt Logan <mlogan5212@gmail.com>
2021-02-01 21:21:40 -0600

    reports wip

--- raterapi.session.sql
+++ raterapi.session.sql
@@ -1,8 +1,20 @@
-                SELECT
-                    gc.id,
-                    c.name CategoryName,
-                    COUNT(g.id) NumberGames
-                FROM raterapi_categories c 
-                JOIN raterapi_game_categories gc ON gc.categories_id = c.id
-                JOIN raterapi_game g ON g.id = gc.game_id
-                GROUP BY c.id
+
+/* GAMES WITH MORE THAN 3 PLAYERS */
+-- SELECT g.title title
+-- FROM raterapi_game g
+-- WHERE g.number_of_players > 3
+
+
+/* WHAT IS THE MOST REVIEWED GAME? */
+-- SELECT MAX(number_of_reviews) review_count, title 
+-- FROM
+-- (SELECT g.title title, COUNT(r.id) number_of_reviews
+-- FROM raterapi_reviews r 
+-- JOIN raterapi_game g on g.id = r.game_id
+-- GROUP BY g.id )
+
+
+-- /* CHILDREN UNDER 8 */
+-- SELECT g.title title
+-- FROM raterapi_game g
+-- WHERE g.age < 8
