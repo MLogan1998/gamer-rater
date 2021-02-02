7c479856a0800a5312a359b6774954cd12997f87
Matt Logan <mlogan5212@gmail.com>
2021-02-01 19:32:07 -0600

    bottom five games report

--- raterreports/urls.py
+++ raterreports/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import top_five_games
+from .views import top_five_games, bottom_five_games
 
 urlpatterns = [
     path('reports/topfive', top_five_games),
+    path('reports/bottomfive', bottom_five_games),
 ]
--- raterreports/views/__init__.py
+++ raterreports/views/__init__.py
@@ -1,2 +1,3 @@
 from .connection import Connection
 from .games.topfivegames import top_five_games
+from .games.bottomfivegames import bottom_five_games
