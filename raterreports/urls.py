d9ace1ceb5add492a65a2381111801a6bf2e23e6
Matt Logan <mlogan5212@gmail.com>
2021-02-03 20:54:07 -0600

    top three users by review count report

--- raterreports/urls.py
+++ raterreports/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import top_five_games, bottom_five_games,games_per_category, more_than_three, most_reviewed_game, under_eight_games
+from .views import top_five_games, bottom_five_games,games_per_category, more_than_three, most_reviewed_game, under_eight_games, top_three_reviewers
 
 urlpatterns = [
     path('reports/topfive', top_five_games),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('reports/morethanthree', more_than_three),
     path('reports/mostreviewed', most_reviewed_game),
     path('reports/undereight', under_eight_games),
+    path('reports/mostreviews', top_three_reviewers),
 ]
--- raterreports/views/__init__.py
+++ raterreports/views/__init__.py
@@ -5,3 +5,4 @@ from .games.gamespercat import games_per_category
 from .games.gamesthreeplayers import more_than_three
 from .games.mostreviewed import most_reviewed_game
 from .games.childrenundereight import under_eight_games
+from .games.topthreereviewers import top_three_reviewers
