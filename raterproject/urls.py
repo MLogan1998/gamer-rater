36474c626d71586d4f8af5c2ef02c2b105877232
Matt Logan <mlogan5212@gmail.com>
2021-02-06 09:56:43 -0600

    add admin path

--- raterproject/urls.py
+++ raterproject/urls.py
@@ -14,6 +14,7 @@ router.register(r'rating', RatingVIewSet, 'rating')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
