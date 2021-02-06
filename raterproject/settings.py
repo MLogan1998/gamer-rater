6210635d530c1afa8d4ee8bbd675aae6f8d040f9
Matt Logan <mlogan5212@gmail.com>
2021-02-06 09:39:49 -0600

    changes to settings

--- raterproject/settings.py
+++ raterproject/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+import os
+import django_on_heroku
+# Build paths in
+# side the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
